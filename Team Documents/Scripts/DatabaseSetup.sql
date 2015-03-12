f8d67376a3251e59b3b90f2c45ad768ab31cd8e2
Slim Babay <slimbabay@hotmail.com>
2015-03-12 01:02:11 +0000

    Created DDL Script
    git-tfs-id: [https://slimbabay.visualstudio.com]$/Project 2 - ENGI3675;C27

---
+++ Team Documents/Scripts/DatabaseSetup.sql
@@ -0,0 +1,35 @@
+﻿
+
+-- Database and permissions setup
+
+drop database if exists "LU.ENGI3675.Proj2";
+
+-- Continue
+
+drop role if exists "DynamicTable";
+create role "DynamicTable" login;
+comment on role "DynamicTable" is 'Restricted IIS app pool user';
+
+-- Run Create Separately
+create database "LU.ENGI3675.Proj2";
+
+-- Continue
+comment on database "LU.ENGI3675.Proj2" is 'Database for ENGI3675 Project 2';
+
+grant connect on database "LU.ENGI3675.Proj2" to "DynamicTable";
+grant connect on database "LU.ENGI3675.Proj2" to "postgres";
+
+
+-- Table setup
+
+drop table if exists Animals Cascade;
+
+create table Animals (
+	Serial_Number serial primary key,
+	Breed text not null check (length(Breed)>0),
+	Weight float not null check (Weight > 0 AND Weight < 500),
+	Age integer not null check (Age > 0 AND Age < 100)
+);
+
+grant select, insert, update, delete on table Animals to "DynamicTable";
+grant select, insert, update, delete on table Animals to "postgres";
