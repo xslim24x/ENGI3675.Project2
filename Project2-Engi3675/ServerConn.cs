96b001214adb8c1c32b692e938e48699be15aa58
Slim Babay <slimbabay@hotmail.com>
2015-03-12 22:22:06 +0000

    Created CRUD interface, commented sandbox for static interface functions
    git-tfs-id: [https://slimbabay.visualstudio.com]$/Project 2 - ENGI3675;C34

--- Project2-Engi3675/ServerConn.cs
+++ Project2-Engi3675/ServerConn.cs
@@ -17,13 +17,43 @@ namespace AnimalData
     }
     public static class ServerConn
     {
-             public static Dictionary<int, Animal> DataDict()
-             {
-             //Lists to hold the values in the DB
-             //List<int> Serial_Number = new List<int>();
-             //List<string> Breed = new List<string>();
-             //List<double> Weight = new List<double>();
-             //List<double> Age = new List<double>();
+        /// <summary>
+        /// Create in CRUD interface
+        /// </summary>
+        /// <param name="a">AnimalClass to be added</param>
+        /// <returns>0 if everythign went ok</returns>
+        public static int DataNew(Animal a)
+        {
+            int problems = 0;
+
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1; Port=5432; Database=ENGI3675.Proj2; Integrated Security=true;");
+
+            try
+            {
+                conn.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO public.\"Animals\"(Breed,Weight,Age) VALUES (:a.AnimalBreed,:a.AnimalWeight,:a.AnimalAge);", conn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            }
+            catch
+            {
+                problems = -1;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return problems;
+        }
+
+        /// <summary>
+        /// Read Database in CRUD interface
+        /// </summary>
+        /// <returns>Dictionary<int,AnimalClass></returns>
+        public static Dictionary<int, Animal> DataDict()
+        {
 
             // create the dictionary accoring to values in the database
             Dictionary<int, Animal> items = new Dictionary<int, Animal>();
@@ -42,12 +72,12 @@ namespace AnimalData
                 // iterate through the columns and add results to the dictionary
                 while (dr.Read())
                 {
-                    
-                    temp.AnimalBreed =  (string)dr["Breed"];
-                    temp.AnimalAge = (double) dr["Age"];
-                    temp.AnimalWeight = (double) dr["Weight"];
-                    temp2.Add((int) dr["Serial_Number"], temp);
-                    
+
+                    temp.AnimalBreed = (string)dr["Breed"];
+                    temp.AnimalAge = (double)dr["Age"];
+                    temp.AnimalWeight = (double)dr["Weight"];
+                    temp2.Add((int)dr["Serial_Number"], temp);
+
                 }
             }
             finally
@@ -57,8 +87,73 @@ namespace AnimalData
 
             return items;
         }
+
+
+        /// <summary
+        /// Update Entry in Database for CRUD interface
+        /// </summary>
+        /// <param name="id">int [Serial ID]</param>
+        /// <param name="a">Animal[Updated Values]</param>
+        /// <returns>0 if everythign went ok</returns>
+        public static int DataUpdate(int id, Animal a)
+        {
+            int problems = 0;
+
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1; Port=5432; Database=ENGI3675.Proj2; Integrated Security=true;");
+
+            try
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("UPDATE public.\"Animals\"(Breed,Weight,Age) VALUES (:a.AnimalBreed,:a.AnimalWeight,:a.AnimalAge) WHERE Serial_Number = :id;", conn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            }
+            catch
+            {
+                problems = -1;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return problems;
+        }
+
+        /// <summary>
+        /// Delete Entry in Database for CRUD interface
+        /// </summary>
+        /// <param name="id">serial id to be deleted</param>
+        /// <returns>0 if everythign went ok</returns>
+        public static int DataDel(int id)
+        {
+            int problems = 0;
+
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1; Port=5432; Database=ENGI3675.Proj2; Integrated Security=true;");
+
+            try
+            {
+                conn.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM public.\"Animals\" WHERE Serial_Number = :id;", conn);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            }
+            catch
+            {
+                problems = -1;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return problems;
+        }
+
+
+
     }
 
 
 }
- 
